243cb1348bc2f3d313c16dfd456371c481b09cd3
madhuridevulapalli <168399503+madhuridevulapalli@users.noreply.github.com>
2025-06-10 21:40:08 +0530

    Add files via upload
    using python programming language i wrote code for temperature calculating.

---
+++ temperature.py
@@ -0,0 +1,22 @@
+"""
+c=int(input("c="))
+f=c*(9/5)+32
+k=273+c
+print(f"Temperature in Fahrenheit:{f}")
+print(f"Temperature in kelvin:{k}")
+"""
+
+temp=int(input("enter temperature:"))
+units=(input("enter units(k or f or c):"))
+c=5/9(f-32)
+f=c*(9/5)+32
+k=273+c
+if c==units:
+    print(f"tempetarure in fahrenheit:{f}\n temperature in kelvin:{k}")
+elif f==units:
+     print(f"tempetarure in celsius:{c}\n temperature in kelvin:{k}")
+else :
+    print(f"tempetarure in celsius:{c}\n temperature in fahrenheit:{f}")
+    
+    
+    
